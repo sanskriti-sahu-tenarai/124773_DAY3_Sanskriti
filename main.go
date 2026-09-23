f749a900bec1227a9fd2071613869f15430c4666
Sanskriti Sahu <Sanskriti.Sahu@tenarai.com>
2026-09-23 16:35:54 +0530

    1st commit

---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import "fmt"
+
+type Employee struct {
+	ID     int
+	Name   string
+	Salary float64
+}
+
+var employees []Employee
+
+func addEmployee(id int, name string, salary float64) {
+	employee := Employee{
+		ID:     id,
+		Name:   name,
+		Salary: salary,
+	}
+
+	employees = append(employees, employee)
+	fmt.Println("Employee added successfully")
+}
+
+func displayEmployees() {
+	fmt.Println("\nEmployee List:")
+
+	for _, employee := range employees {
+		fmt.Printf(
+			"ID: %d | Name: %s | Salary: %.2f\n",
+			employee.ID,
+			employee.Name,
+			employee.Salary,
+		)
+	}
+}
+
+func searchEmployee(id int) {
+	for _, employee := range employees {
+		if employee.ID == id {
+			fmt.Printf(
+				"Employee Found: %d | %s | %.2f\n",
+				employee.ID,
+				employee.Name,
+				employee.Salary,
+			)
+			return
+		}
+	}
+
+	fmt.Println("Employee not found")
+}
+
+func deleteEmployee(id int) {
+	for i, employee := range employees {
+		if employee.ID == id {
+			employees = append(employees[:i], employees[i+1:]...)
+			fmt.Println("Employee deleted successfully")
+			return
+		}
+	}
+
+	fmt.Println("Employee not found")
+}
+
+func main() {
+	addEmployee(101, "Sanskriti", 45000)
+	addEmployee(102, "Rahul", 50000)
+
+	displayEmployees()
+
+	searchEmployee(101)
+
+	deleteEmployee(102)
+
+	displayEmployees()
+}
